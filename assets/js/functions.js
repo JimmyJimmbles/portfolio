4571d9d8666c889300dd636f3ebbd8e9d96c9a7e
Jay Jones <jay@ucidigital.com>
2018-07-24 08:58:57 -0500

    Small changes to parallax mouse movement

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -9,8 +9,6 @@
     l2top = document.getElementById('l2').offsetTop,
     l3Left = document.getElementById('l3').offsetLeft,
     l3top = document.getElementById('l3').offsetTop,
-    l4Left = document.getElementById('l4').offsetLeft,
-    l4top = document.getElementById('l4').offsetTop,
     l5Left = document.getElementById('l5').offsetLeft,
     l5top = document.getElementById('l5').offsetTop,
     l6Left = document.getElementById('l6').offsetLeft,
@@ -18,15 +16,15 @@
 
   pbox.onmousemove = (event) => {
     event = event || window.event;
-    let x = event.clientX - pbox.offsetLeft,
-      y = event.clientY - pbox.offsetTop;
-
-    mouseParallax("l1", l1Left, l1top, x, y, 63);
-    mouseParallax("l2", l2Left, l2top, x, y, 30);
-    mouseParallax("l3", l3Left, l3top, x, y, 17);
-    mouseParallax("l4", l4Left, l4top, x, y, 6);
-    mouseParallax("l5", l5Left, l5top, x, y, 11);
-    mouseParallax("l6", l6Left, l6top, x, y, 48);
+    let x = event.clientX,
+      y = (event.clientY) - 73;
+      console.log("L1"+l1Left);
+      console.log("L1Top"+l1top);
+    mouseParallax("l1", l1Left, l1top, x, y, 23);
+    mouseParallax("l2", l2Left, l2top, x, y, 13);
+    mouseParallax("l3", l3Left, l3top, x, y, 7);
+    mouseParallax("l5", l5Left, l5top, x, y, 5);
+    mouseParallax("l6", l6Left, l6top, x, y, 20);
   }
 })();
 
@@ -36,6 +34,6 @@ function mouseParallax(id, left, top, mouseX, mouseY, speed) {
     containerWidth = parseInt(parantObj.offsetWidth),
     containerHeight = parseInt(parantObj.offsetHeight);
 
-  obj.style.left = left - (mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth * speed + "px";
-  obj.style.top = top - (mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight * speed + "px";
+  obj.style.left = left + (mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth * speed + "px";
+  obj.style.top = top + (mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight * speed + "px";
 }
